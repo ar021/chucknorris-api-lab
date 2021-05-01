4c688cc7ef249a667ba277135afed9a319e6f393
Avesh Rathod <avesh@Aveshs-MacBook-Air.local>
2021-05-01 17:16:23 -0400

    functionality completetd

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const request = require("request");
+const rootURL = "https://api.chucknorris.io/jokes/";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  const category = req.query.category;
+  console.log(category);
+  request(
+    `${rootURL}random?category=${category}`,
+    function (err, response, body) {
+      const jokesData = JSON.parse(body);
+      const jokeCat = jokesData.category;
+      console.log(jokeCat);
+      const joke = jokesData.value;
+      console.log(joke);
+      res.render("index", { jokesData, joke });
+    }
+  );
 });
 
 module.exports = router;
